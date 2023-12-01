b2457cd53ff8295a40a4c81383356cbb93d22d0b
Ebenezer <ebenezer.gyamfi@regent.edu.gh>
2023-11-30 22:49:31 -0800

    WIP

--- app/Http/Integrations/Github/GithubConnector.php
+++ app/Http/Integrations/Github/GithubConnector.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Integrations\Github;
 
+use GuzzleHttp\Psr7\Header;
 use Saloon\Http\Connector;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Saloon\PaginationPlugin\Contracts\HasPagination;
+use Saloon\PaginationPlugin\PagedPaginator;
+use Saloon\PaginationPlugin\Paginator;
 use Saloon\Traits\Plugins\AcceptsJson;
 
-class GithubConnector extends Connector
+use function PHPUnit\Framework\isEmpty;
+
+class GithubConnector extends Connector implements HasPagination
 {
     use AcceptsJson;
 
@@ -23,6 +31,38 @@ class GithubConnector extends Connector
     protected function defaultHeaders(): array
     {
         return [];
+    }
+
+    public function paginate(Request $request): PagedPaginator
+    {
+
+        return new class($this, $request) extends PagedPaginator
+        {
+
+            protected ?int $perPage  = 10;
+
+            protected function isLastPage(Response $response): bool
+            {
+                $linkHeader = Header::parse($response->header('Link'));
+
+
+                return collect($linkHeader)
+                    ->where(fn (array $link): bool => $link['rel'] === 'next')
+                    ->isEmpty();
+            }
+
+
+            protected function getPagedItems(Response $response, Request $request): array
+            {
+                return $response->dtoOrFail()->toArray();
+            }
+
+
+            protected function applyPagination(Request $request) : Request
+            {
+                return $request;
+            }
+        };
     }
 
     /**
