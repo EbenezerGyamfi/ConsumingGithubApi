e492c7ba6fd7b586b066e767dbb58096d2ac2028
Ebenezer <ebenezer.gyamfi@regent.edu.gh>
2023-12-01 08:09:10 -0800

    remove this

--- app/Services/GithubService.php
+++ app/Services/GithubService.php
@@ -15,45 +15,60 @@ use App\Interface\GithubInterface;
 final class GithubService implements GithubInterface
 {
 
-    public function __construct(private string $token)
-    {
-        
-    }
+  public function __construct(private string $token)
+  {
+  }
 
-    public function connector(){
-        return app(GithubConnector::class)->withTokenAuth($this->token);
-    }
+  public function connector()
+  {
+    return app(GithubConnector::class)->withTokenAuth($this->token);
+  }
 
 
-    public function getRepo(string $name,  string $repoName) : Repo
-    {
+public function getRepo(string $name,  string $repoName)
+  {
+
+    $requests = [
+      new GithubRequest('EbenezerGyamfi', 'Taste-Repo-11'),
+      new GithubRequest('EbenezerGyamfi', 'Taste-Repo-10'),
+      new GithubRequest('EbenezerGyamfi', 'Taste-Repo-9'),
+      new GithubRequest('EbenezerGyamfi', 'ChangeRepoNameTest3'),
+      new GithubRequest('EbenezerGyamfi', 'ConsumingGithubApi'),
+      new GithubRequest('EbenezerGyamfi', 'ConsumingGithubApi'),
+      new GithubRequest('EbenezerGyamfi', 'Taste-Repo-11'),
+    ];
+
+    foreach ($requests as $key => $request) {
       return $this->connector()
-      ->send(new GithubRequest($name,$repoName))
-      ->dtoOrFail();
+        ->send($request)
+        ->json();
     }
+  }
 
 
-    public function getLanguages(string $name, string $repoName): array{
+  public function getLanguages(string $name, string $repoName): array
+  {
 
-      return $this->connector()
-             ->send(new GithubLanguageRequest( userName:$name, repoName:$repoName))
-             ->json();
-         
-    }
+    return $this->connector()
+      ->send(new GithubLanguageRequest(userName: $name, repoName: $repoName))
+      ->json();
+  }
 
 
-    public function createRepo(NewRepoData $newRepoData) : Repo{
-      return $this->connector()
+  public function createRepo(NewRepoData $newRepoData): Repo
+  {
+    return $this->connector()
       ->send(new GithubCreateRepoRequest($newRepoData))
       ->dtoOrFail();
-    }
+  }
 
 
-    public function updateRepo(string $repoName, string $owner,  UpdateRepo $updateRepo): Repo{
+  public function updateRepo(string $repoName, string $owner,  UpdateRepo $updateRepo): Repo
+  {
 
 
-      return $this->connector()
-          ->send(new GithubUpdateRepoRequest(updateRepo: $updateRepo, owner: $owner, repoName: $repoName))
-          ->dtoOrFail();
-    }
+    return $this->connector()
+      ->send(new GithubUpdateRepoRequest(updateRepo: $updateRepo, owner: $owner, repoName: $repoName))
+      ->dtoOrFail();
+  }
 }
