7ebcf07fb2916dce5f1b5ed9af54608edfe6554d
Ebenezer <ebenezer.gyamfi@regent.edu.gh>
2023-12-14 12:59:41 -0800

    WIP

--- app/Services/GithubService.php
+++ app/Services/GithubService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use App\Collection\Repocollection;
@@ -14,15 +15,16 @@ use App\Http\Integrations\Github\Requests\GetRepoRequest;
 use App\Http\Integrations\Github\Requests\UpdateRepoRequest;
 use App\Interfaces\GithubInterface;
 
-final class GithubService implements GithubInterface {
+final class GithubService implements GithubInterface
+{
 
     public function __construct(private readonly string $token)
     {
-        
     }
 
 
-    public function connnector(): GithubConnector{
+    public function connnector(): GithubConnector
+    {
 
         return app(GithubConnector::class)->withTokenAuth($this->token);
     }
@@ -32,35 +34,39 @@ final class GithubService implements GithubInterface {
     public function getRepos(): Repocollection
     {
         $repos  =  $this->connnector()
-                        ->paginate(new GetAuthUserRepos)
-                        ->collect()
-                        ->ensure(Repo::class);
-            
-            return Repocollection::make($repos);
+            ->paginate(new GetAuthUserRepos);
+
+        $repos->setPerPageLimit(50);
+
+
+        $results =  $repos
+            ->collect()
+            ->ensure(Repo::class);
+
+        return Repocollection::make($results);
     }
 
 
     public function getRepo(string $repoName, string $owner): Repo
     {
         return $this->connnector()
-        ->send(new GetRepoRequest(repoName: $repoName, owner: $owner))
-        ->dtoOrFail();
+            ->send(new GetRepoRequest(repoName: $repoName, owner: $owner))
+            ->dtoOrFail();
     }
 
     public function updateRepo(string $repoName, string $owner, UpdateRepoData $updateRepoData): Repo
     {
 
         return $this->connnector()
-                    ->send(new UpdateRepoRequest(repoName: $repoName, owner: $owner, updateRepoData: $updateRepoData))
-                    ->dtoOrFail();
+            ->send(new UpdateRepoRequest(repoName: $repoName, owner: $owner, updateRepoData: $updateRepoData))
+            ->dtoOrFail();
     }
 
     public function createRepo(NewRepoData $newRepoData): Repo
     {
         return $this->connnector()
-                    ->send(new CreateGithubRepo($newRepoData))
-                    ->dtoOrFail();
-                    
+            ->send(new CreateGithubRepo($newRepoData))
+            ->dtoOrFail();
     }
 
 
@@ -74,20 +80,11 @@ final class GithubService implements GithubInterface {
 
     public function getRepoLanguages(string $repoName, string $owner): array
     {
-        
+
         return $this->connnector()
-                    ->send(new GetrepoLanguages(repoName: $repoName, owner: $owner))
-                    ->collect()
-                    ->keys()
-                    ->all();
-
+            ->send(new GetrepoLanguages(repoName: $repoName, owner: $owner))
+            ->collect()
+            ->keys()
+            ->all();
     }
-
-
-    
-
-
-
-
-
 }
